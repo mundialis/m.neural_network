e13e53f663ca8581b22173cf8db96e52f29c0015
Guido Riembauer <62383722+griembauer@users.noreply.github.com>
2025-01-14 16:04:00 +0100

    Update m.neural_network.preparetraining/m.neural_network.preparetraining.py
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- m.neural_network.preparetraining/m.neural_network.preparetraining.py
+++ m.neural_network.preparetraining/m.neural_network.preparetraining.py
@@ -155,7 +155,7 @@ def vrt_absolute_paths(vrt, abs_paths, rel_paths):
     :param abs_paths: List: absolute paths to be replaced
     :param rel_paths: List: relative paths to replace the absolute paths with.
     """
-    with open(vrt, "r") as file:
+    with open(vrt, encoding="utf-8") as file:
         data = file.read()
         data = data.replace('relativeToVRT="0"', 'relativeToVRT="1"')
         for abs_path, rel_path in zip(abs_paths, rel_paths):
