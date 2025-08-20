e423c8451ba561af8619643988ab9a4e26d7bd3a
Lina Krisztian <krisztian@mundialis.de>
2025-08-20 13:43:28 +0200

    linting

--- m.neural_network.postprocessing_patch/m.neural_network.postprocessing_patch.py
+++ m.neural_network.postprocessing_patch/m.neural_network.postprocessing_patch.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
-"""
-############################################################################
+"""############################################################################
 #
 # MODULE:      m.neural_network.postprocessing_patch
-# AUTHOR(S):   Lina Krisztian
+# AUTHOR(S):   Lina Krisztian.
 
-# PURPOSE:     Patches tiles resulting from neural network inference
+# PURPOSE:     Patches tiles resulting from neural network inference.
 # COPYRIGHT:   (C) 2025 by mundialis GmbH & Co. KG and the GRASS Development
 #              Team
 #
@@ -30,7 +29,7 @@
 # %option G_OPT_F_INPUT
 # % key: tiles_filelist
 # % required: no
-# % description: list of tiles, which should be patched (filename) (if not files within tiles_path should be used)
+# % description: list of tiles, which should be patched (filename) (if not all files within tiles_path should be used)
 # %end
 
 # %option G_OPT_M_DIR
@@ -63,26 +62,26 @@ import atexit
 import os
 
 import grass.script as grass
-
 from grass_gis_helpers.cleanup import general_cleanup
 
 # initialize global variables
 ID = grass.tempname(12)
-orig_region = None
+ORIG_REGION = None
 rm_rasters = []
 
 
 # cleanup function
 def cleanup():
-    """Cleanup fuction"""
+    """Cleanup fuction."""
     general_cleanup(
-        orig_region = orig_region,
-        rm_rasters = rm_rasters,
+        orig_region=ORIG_REGION,
+        rm_rasters=rm_rasters,
     )
 
+
 def main():
-    """Main function of r.postprocessing.patch"""
-    global ID, orig_region, rm_rasters
+    """r.postprocessing.patch main function."""
+    global ID, ORIG_REGION, rm_rasters
 
     tiles_filelist = options["tiles_filelist"]
     tiles_path = options["tiles_path"]
@@ -91,16 +90,16 @@ def main():
     keep_border_tile_edges = flags["b"]
 
     # save original region
-    orig_region = f"original_region_{ID}"
-    grass.run_command("g.region", save=orig_region, quiet=True)
+    ORIG_REGION = f"original_region_{ID}"
+    grass.run_command("g.region", save=ORIG_REGION, quiet=True)
 
     # Read all files into a list
     if tiles_filelist:
-        with open(tiles_filelist, "r") as file:
+        with open(tiles_filelist, encoding="utf-8") as file:
             tiles_list = [line.strip() for line in file]
     else:
         tiles_list = [f for f in os.listdir(tiles_path) if f.endswith(".tif")]
-    
+
     grass.message(_("Importing tiles and cutting of edges ..."))
     rast_list = []
     for tiles in tiles_list:
@@ -109,27 +108,27 @@ def main():
         # Import data
         grass.run_command(
             "r.import",
-            input = os.path.join(tiles_path, tiles),
-            output = tiles_rast,
+            input=os.path.join(tiles_path, tiles),
+            output=tiles_rast,
         )
         # Cut edges
         grass.run_command(
             "g.region",
-            raster = tiles_rast,
+            raster=tiles_rast,
             # todo: set dest_res if given
         )
         grass.run_command(
             "g.region",
-            n = f"n-{edge_cut}",
-            s = f"s+{edge_cut}",
-            e = f"e-{edge_cut}",
-            w = f"w+{edge_cut}",
+            n=f"n-{edge_cut}",
+            s=f"s+{edge_cut}",
+            e=f"e-{edge_cut}",
+            w=f"w+{edge_cut}",
         )
         tiles_rast_cut = tiles.split(".")[0]
         rm_rasters.append(tiles_rast_cut)
         grass.run_command(
             "r.mapcalc",
-            expression = f"{tiles_rast_cut} = {tiles_rast}",
+            expression=f"{tiles_rast_cut} = {tiles_rast}",
         )
         rast_list.append(tiles_rast_cut)
 
@@ -140,30 +139,27 @@ def main():
         patch_out = f"{output}_without_border_tile_edges_{ID}"
         rm_rasters.append(patch_out)
     # region to all tiles
-    grass.run_command(
-        "g.region",
-        raster = rast_list
-    )
+    grass.run_command("g.region", raster=rast_list)
     grass.message(_("Patching tiles ..."))
     grass.run_command(
         "r.patch",
-        input = rast_list,
-        output = patch_out,
+        input=rast_list,
+        output=patch_out,
     )
 
     # If edges of border tiles should be kept
     if keep_border_tile_edges:
         # add width of cutted tile edges to region
         grass.run_command(
-           "g.region",
-            n = f"n+{edge_cut}",
-            s = f"s-{edge_cut}",
-            e = f"e+{edge_cut}",
-            w = f"w-{edge_cut}",
+            "g.region",
+            n=f"n+{edge_cut}",
+            s=f"s-{edge_cut}",
+            e=f"e+{edge_cut}",
+            w=f"w-{edge_cut}",
         )
         tmpfile = grass.tempfile()
         # Create file for input to buildvrt, in case of many input files
-        with open(tmpfile, 'w') as f:
+        with open(tmpfile, "w", encoding="utf-8") as f:
             for rast in rast_list:
                 f.write(f"{rast}_tmp\n")
         buildvrt_out = f"vrt_all_no_edges_cut_{ID}"
@@ -171,14 +167,14 @@ def main():
         grass.message(_("Creating VRT without cutted edges ..."))
         grass.run_command(
             "r.buildvrt",
-            file = tmpfile,
-            output = buildvrt_out,
+            file=tmpfile,
+            output=buildvrt_out,
         )
         grass.message(_("Patching tiles, while keeping border edges..."))
         grass.run_command(
             "r.patch",
-            input = f"{patch_out},{buildvrt_out}",
-            output = output
+            input=f"{patch_out},{buildvrt_out}",
+            output=output,
         )
 
 
