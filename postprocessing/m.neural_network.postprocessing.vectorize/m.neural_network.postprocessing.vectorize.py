8a0ca16f9abe396dfc456dbee2334b659b2e631b
linakrisztian <106728040+linakrisztian@users.noreply.github.com>
2025-09-22 18:57:10 +0200

    Update postprocessing/m.neural_network.postprocessing.vectorize/m.neural_network.postprocessing.vectorize.py
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- postprocessing/m.neural_network.postprocessing.vectorize/m.neural_network.postprocessing.vectorize.py
+++ postprocessing/m.neural_network.postprocessing.vectorize/m.neural_network.postprocessing.vectorize.py
@@ -79,7 +79,7 @@ rm_vectors = []
 
 # cleanup function
 def cleanup():
-    """Cleanup fuction"""
+    """Cleanup fuction."""
     general_cleanup(
         orig_region=orig_region,
         rm_rasters=rm_rasters,
