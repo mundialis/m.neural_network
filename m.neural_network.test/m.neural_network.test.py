9048d9feac3ca9586e1ea96f6bf6cb11e6af3e06
Lina Krisztian <krisztian@mundialis.de>
2025-09-22 15:00:41 +0200

    usage of geo_neural_network lib and update of requirements.txt

--- m.neural_network.test/m.neural_network.test.py
+++ m.neural_network.test/m.neural_network.test.py
@@ -61,14 +61,8 @@
 
 import grass.script as grass
 
-# import module library
-grass.utils.set_path(
-    modulename="m.neural_network",
-    dirname="smp_lib",
-    path="..",
-)
 # pylint: disable=C0413
-from smp_lib.smp_test import smp_test
+from geo_neural_network.smp_lib.smp_test import smp_test
 
 
 def main():
