38b8156a766a83fbb4cb5b9ae378da101586c171
Lina Krisztian <krisztian@mundialis.de>
2025-09-24 17:38:11 +0200

    cleanup scripts

--- tests/small_example_script.sh
+++ tests/small_example_script.sh
@@ -37,22 +37,22 @@ v.import input=${PATH_TO_TESTDATA}/input/ref_data_trees.gpkg output=ref_data_tre
 
 # Data preparation
 
-m.neural_network.preparedata_part1 image_bands=dop.1,dop.2,dop.3,dop.4 ndsm=ndsm output_dir=${PATH_TO_TESTDATA}/output/preparedata_part1 || errormsg "ERROR: m.neural_network.preparedata_part1 failed"
+m.neural_network.preparedata_part1 image_bands=dop.1,dop.2,dop.3,dop.4 ndsm=ndsm output_dir=${PATH_TO_TESTDATA}/output/preparedata_part1 || errormsg "m.neural_network.preparedata_part1 failed"
 
-m.neural_network.preparedata_part2 input_traindir=${PATH_TO_TESTDATA}/input/preparedata_part1/train input_applydir=${PATH_TO_TESTDATA}/output/preparedata_part1/apply test_percentage=10 output=${PATH_TO_TESTDATA}/output/preparedata_part2 || errormsg "ERROR: m.neural_network.preparedata_part2 failed"
+m.neural_network.preparedata_part2 input_traindir=${PATH_TO_TESTDATA}/input/preparedata_part1/train input_applydir=${PATH_TO_TESTDATA}/output/preparedata_part1/apply test_percentage=10 output=${PATH_TO_TESTDATA}/output/preparedata_part2 || errormsg "m.neural_network.preparedata_part2 failed"
 
 # Model Training and evaluation
 
-m.neural_network.train data_dir=${PATH_TO_TESTDATA}/output/preparedata_part2/train output_model_path=${PATH_TO_TESTDATA}/output/model output_train_metrics_path=${PATH_TO_TESTDATA}/output/model_train_metrics epochs=2 || errormsg "ERROR: m.neural_network.train failed"
+m.neural_network.train data_dir=${PATH_TO_TESTDATA}/output/preparedata_part2/train output_model_path=${PATH_TO_TESTDATA}/output/model output_train_metrics_path=${PATH_TO_TESTDATA}/output/model_train_metrics epochs=2 || errormsg "m.neural_network.train failed"
 
-m.neural_network.test data_dir=${PATH_TO_TESTDATA}/output/preparedata_part2/train input_model_path=${PATH_TO_TESTDATA}/input/example_model output_path=${PATH_TO_TESTDATA}/output/model_testdata_evaluation || errormsg "ERROR: m.neural_network.test failed"
+m.neural_network.test data_dir=${PATH_TO_TESTDATA}/output/preparedata_part2/train input_model_path=${PATH_TO_TESTDATA}/input/example_model output_path=${PATH_TO_TESTDATA}/output/model_testdata_evaluation || errormsg "m.neural_network.test failed"
 
-m.neural_network.apply data_dir=${PATH_TO_TESTDATA}/input/preparedata_part2/apply input_model_path=${PATH_TO_TESTDATA}/input/example_model/ output_path=${PATH_TO_TESTDATA}/output/applied_model || errormsg "ERROR: m.neural_network.apply failed"
+m.neural_network.apply data_dir=${PATH_TO_TESTDATA}/input/preparedata_part2/apply input_model_path=${PATH_TO_TESTDATA}/input/example_model/ output_path=${PATH_TO_TESTDATA}/output/applied_model || errormsg "m.neural_network.apply failed"
 
 # Postprocessing
 
-m.neural_network.postprocessing.patch tiles_path=${PATH_TO_TESTDATA}/output/applied_model output=applied_model_raster  || errormsg "ERROR: m.neural_network.postprocessing.patch failed"
+m.neural_network.postprocessing.patch tiles_path=${PATH_TO_TESTDATA}/output/applied_model output=applied_model_raster  || errormsg "m.neural_network.postprocessing.patch failed"
 
-m.neural_network.postprocessing.vectorize input=applied_model_raster output=applied_model_vector || errormsg "ERROR: m.neural_network.postprocessing.vectorize failed"
+m.neural_network.postprocessing.vectorize input=applied_model_raster output=applied_model_vector || errormsg "m.neural_network.postprocessing.vectorize failed"
 
-m.neural_network.postprocessing.snapref a_input_classification=applied_model_vector b_input_reference=ref_data_trees output=applied_model_vector_cleaned  merge_col=cat rmarea_thres_inside=20 rmarea_where_inside="b_cat" rmarea_thres_outside=20 rmarea_where_outside="b_cat" || errormsg "ERROR: m.neural_network.postprocessing.snapref failed"
+m.neural_network.postprocessing.snapref a_input_classification=applied_model_vector b_input_reference=ref_data_trees output=applied_model_vector_cleaned  merge_col=cat rmarea_thres_inside=20 rmarea_where_inside="b_cat" rmarea_thres_outside=20 rmarea_where_outside="b_cat" || errormsg "m.neural_network.postprocessing.snapref failed"
