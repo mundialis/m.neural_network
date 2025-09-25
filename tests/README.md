34307810d254c56c0ea7e8742f8b674c25b16531
Lina Krisztian <krisztian@mundialis.de>
2025-09-25 12:24:19 +0200

    add testdata download + documentation

--- tests/README.md
+++ tests/README.md
@@ -1,7 +1,8 @@
 # Small example script for m.neural_network
 
-- Required test data available here: TODO
+- Required test data available here: https://apps.mundialis.de/testdata/testdata_for_small_example_script.zip
+  - Note: will be downloaded automatically within [small_example_script.sh](./small_example_script.sh)
 - Within [small_example_script.sh](./small_example_script.sh) a small setup is given for testing the addons of m.neural_network
-  - For running the script, first download the sample data (see above), and adjust the path within the script
+  - For running the script, first adjust the output path (for the testdata) within the script
   - Then run the script within a GRASS GIS session
-- Note: For reasonable testing the output of the Addons is not always forwarded to the next one. In some cases, the already prepared testdata (e.g. labeled data, trained model, etc.) are used.
+- Note: In some cases, for reasonable testing, already prepared testdata (e.g. labeled data, trained model, etc.) are used. Therefore, the output of the Addons is not always forwarded to the next Addon.
--- tests/small_example_script.sh
+++ tests/small_example_script.sh
@@ -2,14 +2,16 @@
 #
 # Small example script, for testing the m.neural_network Addons
 
-# For testdata see also README.md within this folder
-
-# TODO: adjust path
-PATH_TO_TESTDATA=/path/to/testdata/
+# TODO: adjust output path
+PATH_TO_TESTDATA=/path/for/downloaded/testdata/
 
 # ---- Don't change anything below
 
 # -- Setup
+
+# Get testdata
+wget https://apps.mundialis.de/testdata/testdata_for_small_example_script.zip -P ${PATH_TO_TESTDATA} --continue
+unzip ${PATH_TO_TESTDATA}/testdata_for_small_example_script.zip -d ${PATH_TO_TESTDATA}
 
 # Check if in  GRASS session
 if test "$GISBASE" = ""; then
@@ -20,7 +22,7 @@ fi
 export GRASS_OVERWRITE=1
 
 # Cleanup/Remove results from previous runs
-rm ${PATH_TO_TESTDATA}/output -rf
+rm ${PATH_TO_TESTDATA}/testdata/output -rf
 
 errormsg()
 {
@@ -28,30 +30,30 @@ errormsg()
     exit 1
 }
 
-r.import input=${PATH_TO_TESTDATA}/input/dop.tif output=dop
-r.import input=${PATH_TO_TESTDATA}/input/ndsm.tif output=ndsm
+r.import input=${PATH_TO_TESTDATA}/testdata/input/dop.tif output=dop
+r.import input=${PATH_TO_TESTDATA}/testdata/input/ndsm.tif output=ndsm
 g.region raster=dop.1 -p
-v.import input=${PATH_TO_TESTDATA}/input/ref_data_trees.gpkg output=ref_data_trees
+v.import input=${PATH_TO_TESTDATA}/testdata/input/ref_data_trees.gpkg output=ref_data_trees
 
 # -- Test Addons
 
 # Data preparation
 
-m.neural_network.preparedata_part1 image_bands=dop.1,dop.2,dop.3,dop.4 ndsm=ndsm output_dir=${PATH_TO_TESTDATA}/output/preparedata_part1 || errormsg "m.neural_network.preparedata_part1 failed"
+m.neural_network.preparedata_part1 image_bands=dop.1,dop.2,dop.3,dop.4 ndsm=ndsm output_dir=${PATH_TO_TESTDATA}/testdata/output/preparedata_part1 || errormsg "m.neural_network.preparedata_part1 failed"
 
-m.neural_network.preparedata_part2 input_traindir=${PATH_TO_TESTDATA}/input/preparedata_part1/train input_applydir=${PATH_TO_TESTDATA}/output/preparedata_part1/apply test_percentage=10 output=${PATH_TO_TESTDATA}/output/preparedata_part2 || errormsg "m.neural_network.preparedata_part2 failed"
+m.neural_network.preparedata_part2 input_traindir=${PATH_TO_TESTDATA}/testdata/input/preparedata_part1/train input_applydir=${PATH_TO_TESTDATA}/testdata/output/preparedata_part1/apply test_percentage=10 output=${PATH_TO_TESTDATA}/testdata/output/preparedata_part2 || errormsg "m.neural_network.preparedata_part2 failed"
 
 # Model Training and evaluation
 
-m.neural_network.train data_dir=${PATH_TO_TESTDATA}/output/preparedata_part2/train output_model_path=${PATH_TO_TESTDATA}/output/model output_train_metrics_path=${PATH_TO_TESTDATA}/output/model_train_metrics epochs=2 || errormsg "m.neural_network.train failed"
+m.neural_network.train data_dir=${PATH_TO_TESTDATA}/testdata/output/preparedata_part2/train output_model_path=${PATH_TO_TESTDATA}/testdata/output/model output_train_metrics_path=${PATH_TO_TESTDATA}/testdata/output/model_train_metrics epochs=2 || errormsg "m.neural_network.train failed"
 
-m.neural_network.test data_dir=${PATH_TO_TESTDATA}/output/preparedata_part2/train input_model_path=${PATH_TO_TESTDATA}/input/example_model output_path=${PATH_TO_TESTDATA}/output/model_testdata_evaluation || errormsg "m.neural_network.test failed"
+m.neural_network.test data_dir=${PATH_TO_TESTDATA}/testdata/output/preparedata_part2/train input_model_path=${PATH_TO_TESTDATA}/testdata/input/example_model output_path=${PATH_TO_TESTDATA}/testdata/output/model_testdata_evaluation || errormsg "m.neural_network.test failed"
 
-m.neural_network.apply data_dir=${PATH_TO_TESTDATA}/input/preparedata_part2/apply input_model_path=${PATH_TO_TESTDATA}/input/example_model/ output_path=${PATH_TO_TESTDATA}/output/applied_model || errormsg "m.neural_network.apply failed"
+m.neural_network.apply data_dir=${PATH_TO_TESTDATA}/testdata/input/preparedata_part2/apply input_model_path=${PATH_TO_TESTDATA}/testdata/input/example_model/ output_path=${PATH_TO_TESTDATA}/testdata/output/applied_model || errormsg "m.neural_network.apply failed"
 
 # Postprocessing
 
-m.neural_network.postprocessing.patch tiles_path=${PATH_TO_TESTDATA}/output/applied_model output=applied_model_raster  || errormsg "m.neural_network.postprocessing.patch failed"
+m.neural_network.postprocessing.patch tiles_path=${PATH_TO_TESTDATA}/testdata/output/applied_model output=applied_model_raster  || errormsg "m.neural_network.postprocessing.patch failed"
 
 m.neural_network.postprocessing.vectorize input=applied_model_raster output=applied_model_vector || errormsg "m.neural_network.postprocessing.vectorize failed"
 
