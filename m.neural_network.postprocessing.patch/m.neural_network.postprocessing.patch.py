daaaf7fe0be5a2e3afb263d2dd831019db64c144
Markus Metz <markus.metz.giswork@gmail.com>
2025-12-10 17:43:59 +0100

    imperative mood

--- m.neural_network.postprocessing.patch/m.neural_network.postprocessing.patch.py
+++ m.neural_network.postprocessing.patch/m.neural_network.postprocessing.patch.py
@@ -87,7 +87,7 @@ def cleanup():
 
 
 def main():
-    """Main function for patching."""
+    """Import, clean, clip and patch tiles."""
     global ORIG_REGION
 
     tiles_filelist = options["tiles_filelist"]
